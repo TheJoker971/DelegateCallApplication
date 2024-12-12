bd91f9d1b776d4738b6d111e476ef2d5a73dd121
TheJoker971 <jojo.blsegor@gmail.com>
2024-12-12 13:51:15 +0100

    Initial commit

--- test/World.t.sol
+++ test/World.t.sol
@@ -3,16 +3,15 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {World} from "../src/World.sol";
+import {Proxy} from "../src/Proxy.sol";
 
 contract WorldTest is Test {
     World public world;
+    Proxy public proxy;
 
     function setUp() public {
-        world = new World("Joker Paradise","PRDZ");
+        
     }
 
-    function TestMint() public {
-        world.mint("Gotham",0,0);
-        assertEq(world.getProperty(0).name,"Gotham");
-    }
+    
 }
